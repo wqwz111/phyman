44b7e35c760b1ca4d743ddfc811c5d4d3db87998
wqwz111 <wqwz111@gmail.com>
2016-04-09 22:30:27 +0800

    email settings

--- modules/settings/js/SettingsController.js
+++ modules/settings/js/SettingsController.js
@@ -1,17 +1,30 @@
 angular.module('phyman.settings',['ngMaterial'])
-  .controller('SettingsCtrl',['$rootScope','$scope','$mdToast','$state','AuthService',
-    function($rootScope,$scope,$mdToast,$state,AuthService) {
+  .controller('SettingsCtrl',['$rootScope','$scope','$http','$mdToast','$state','AuthService',
+    function($rootScope,$scope,$http,$mdToast,$state,AuthService) {
     $scope.onResetPasswdClick = function () {
-        AuthService.resetPassword($scope.password)
-          .then(function(response) {
-            $mdToast.show(
-                $mdToast.simple()
-                  .textContent('密码修改成功')
-                  .hideDelay(2000)
-            );
-            $state.go('index');
-          },function(error) {
+      AuthService.resetPassword($scope.password)
+        .then(function(response) {
+          $mdToast.show(
+            $mdToast.simple()
+              .textContent('密码修改成功')
+              .hideDelay(1500)
+          );
+          $state.go('settings');
+        },function(error) {
 
-          });
+        });
     };
+    $scope.onUpdateEmailClick = function() {
+      $http.post($rootScope.API_HOST + '/update_email',{
+        email: $scope.email
+      })
+      .then(function(response) {
+        $mdToast.show(
+          $mdToast.simple()
+            .textContent('Email修改成功')
+            .hideDelay(1500)
+        );
+        $state.go('settings');
+      },function(error) {});
+    }
   }])
--- modules/settings/js/SettingsRoute.js
+++ modules/settings/js/SettingsRoute.js
@@ -5,15 +5,26 @@ angular.module('phyman.settings')
   			url: '/settings',
   			templateUrl: 'views/settings.html'
   		})
-        .state('settings.reset_password',{
-            url: '/reset_password',
-            templateUrl: 'views/reset_password.html',
-            controller: 'SettingsCtrl',
-            data: {
-                permissions: {
-                    except: ['anonymous'],
-                    redirectTo: 'error'
-                }
-            }
-        });
+      .state('settings.reset_password',{
+          url: '/reset_password',
+          templateUrl: 'views/reset_password.html',
+          controller: 'SettingsCtrl',
+          data: {
+              permissions: {
+                  except: ['anonymous'],
+                  redirectTo: 'error'
+              }
+          }
+      })
+      .state('settings.update_email',{
+          url: '/update_email',
+          templateUrl: 'views/update_email.html',
+          controller: 'SettingsCtrl',
+          data: {
+              permissions: {
+                  except: ['anonymous'],
+                  redirectTo: 'error'
+              }
+          }
+      });
   }])
