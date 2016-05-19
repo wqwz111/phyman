fcc0f047b0b6145e1da02fbbd22b7259f8e856f3
Xiaoyi Lin <ivy.java@gmail.com>
2016-05-19 14:04:22 +0800

    fix

--- modules/settings/js/SettingsController.js
+++ modules/settings/js/SettingsController.js
@@ -1,30 +1,20 @@
 angular.module('phyman.settings',['ngMaterial'])
-  .controller('SettingsCtrl',['$rootScope','$scope','$http','$mdToast','$state','AuthService',
-    function($rootScope,$scope,$http,$mdToast,$state,AuthService) {
+  .controller('SettingsCtrl',['$rootScope','$scope','$mdToast','$state','AuthService',
+    function($rootScope,$scope,$mdToast,$state,AuthService) {
     $scope.onResetPasswdClick = function () {
-      AuthService.resetPassword($scope.password)
-        .then(function(response) {
-          $mdToast.show(
-            $mdToast.simple()
-              .textContent('密码修改成功')
-              .hideDelay(1500)
-          );
-          $state.go('settings');
-        },function(error) {
+        AuthService.resetPassword($scope.password)
+          .then(function(response) {
+            $mdToast.show(
+                $mdToast.simple()
+                  .textContent('密码修改成功,请重新登录')
+                  .hideDelay(2000)
+            );
+            AuthService.logout();
+            $rootScope.isLoggedIn = false;
+            $state.go('index',null,{reload:true});
+            
+          },function(error) {
 
-        });
+          });
     };
-    $scope.onUpdateEmailClick = function() {
-      $http.post($rootScope.API_HOST + '/update_email',{
-        email: $scope.email
-      })
-      .then(function(response) {
-        $mdToast.show(
-          $mdToast.simple()
-            .textContent('Email修改成功')
-            .hideDelay(1500)
-        );
-        $state.go('settings');
-      },function(error) {});
-    }
   }])
